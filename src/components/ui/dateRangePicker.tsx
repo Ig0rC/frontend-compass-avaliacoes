3a366d90f7a8c68777e10c57c5f03d0a75157c53
Igor Cardoso <49351266+Ig0rC@users.noreply.github.com>
2024-12-24 08:41:32 -0300

    deploy

--- src/components/ui/dateRangePicker.tsx
+++ src/components/ui/dateRangePicker.tsx
@@ -17,7 +17,7 @@ import { date } from "zod"
 
 interface Props {
   className?: string;
-  value?: DateRange | undefined;
+  value: DateRange | undefined;
   onChange: (date: DateRange | undefined) => void;
 }
 
